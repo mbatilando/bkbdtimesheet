efca03f1c407305be1164fff0d080a2e5b6660bc
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-12 12:16:07 -0800

    Created django files to allow hosting on heroku

---
+++ bkbdtimesheet_backend/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import patterns, include, url
+from bkbdtimesheet_backend.views import index, login
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	('^$', index),
+	(r'^index/$', index),
+	(r'^login/$', login),
+    # Examples:
+    # url(r'^$', 'bkbdtimesheet_backend.views.home', name='home'),
+    # url(r'^bkbdtimesheet_backend/', include('bkbdtimesheet_backend.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+#urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+	urlpatterns += patterns('django.contrib.staticfiles.views', url(r'^asstes/(?P<path>.*)$', 'serve'),
+	)
---
+++ bkbdtimesheet_backend/views.py
@@ -0,0 +1,16 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response
+
+def index(request):
+	#t = get_template('index.html')
+	#html = t.render(Context())
+	#return HttpResponse(html)
+	return render_to_response('index.html')
+	
+def login(request):
+	#t = get_template('login.html')
+	#html = t.render(Context())
+	#return HttpResponse(html)
+	return render_to_response('login.html')
