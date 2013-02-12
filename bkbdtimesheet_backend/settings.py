0ba0a3f637fbdd748f81bcb1db230081f5d00f9f
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-12 12:20:43 -0800

    added another directory for templates

--- bkbdtimesheet_backend/settings.py
+++ bkbdtimesheet_backend/settings.py
@@ -111,6 +111,7 @@ WSGI_APPLICATION = 'bkbdtimesheet_backend.wsgi.application'
 
 TEMPLATE_DIRS = (
 	"C:/Users/oscar.bachtiar/Documents/Oscar/bkbdtimesheet",
+	"/apps",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
