c2447d87279e72e95451d60b8e74cb71b76fcfcd
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-15 14:36:47 -0800

    Added @csrf_exempt

--- bkbdtimesheet_backend/views.py
+++ bkbdtimesheet_backend/views.py
@@ -3,17 +3,20 @@ from django.template import Context
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 
+@csrf_exempt
 def index(request):
 	#t = get_template('index.html')
 	#html = t.render(Context())
 	#return HttpResponse(html)
 	return render_to_response('index.html')
-	
+
+@csrf_exempt	
 def login(request):
 	#t = get_template('login.html')
 	#html = t.render(Context())
 	#return HttpResponse(html)
 	return render_to_response('login.html')
-	
+
+@csrf_exempt	
 def submit(request):
 	return HttpResponse("This works")
