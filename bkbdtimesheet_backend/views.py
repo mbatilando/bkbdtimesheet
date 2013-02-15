5b337c8891278038e88782eececbe83adca8060e
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-15 15:42:19 -0800

    Added e-mail functionality

--- bkbdtimesheet_backend/views.py
+++ bkbdtimesheet_backend/views.py
@@ -6,6 +6,14 @@ from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
 import csv
 
+#e-mail stuff
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
 @csrf_exempt
 def index(request):
 	#t = get_template('index.html')
@@ -42,4 +50,33 @@ def submit(request):
 			writer.writerow([day, timeIn, lunchIn, lunchOut, timeOut, timeTotal])
 		writer.writerow(['','','','','', grandTotal])
 		
-	return HttpResponse(open('output.csv'))
+	#Send the result via e-mail
+	mail("oscarbachtiar759@gmail.com", "Blitzshadow759", "oscarbachtiar759@gmail.com", "timesheets " + str(requestInput['weekof']), "Total hours: " + grandTotal, "output.csv")
+		
+	return HttpResponse("Submitted, time to pop some tags")
+	
+@csrf_exempt
+def mail(gmail_user, gmail_pwd, to, subject, text, attach):
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   part = MIMEBase('application', 'octet-stream')
+   part.set_payload(open(attach, 'rb').read())
+   Encoders.encode_base64(part)
+   part.add_header('Content-Disposition',
+           'attachment; filename="%s"' % os.path.basename(attach))
+   msg.attach(part)
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   # Should be mailServer.quit(), but that crashes...
+   mailServer.close()
