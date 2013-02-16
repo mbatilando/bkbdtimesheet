42d9b98b9f17af8826d01d3f65d8fed25fd2dd14
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-15 16:10:03 -0800

    changed yahoo servel credentials

--- bkbdtimesheet_backend/views.py
+++ bkbdtimesheet_backend/views.py
@@ -72,7 +72,7 @@ def mail(gmail_user, gmail_pwd, to, subject, text, attach):
            'attachment; filename="%s"' % os.path.basename(attach))
    msg.attach(part)
 
-   mailServer = smtplib.SMTP("smtp.yahoo.com", 587)
+   mailServer = smtplib.SMTP("smtp.mail.yahoo.com", 465)
    mailServer.ehlo()
    mailServer.starttls()
    mailServer.ehlo()
