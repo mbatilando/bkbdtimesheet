6e38db1d34fc6a4cc993336eb364a87ffcccd688
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-19 15:01:03 -0800

    make the written values equal to actual times, as opposed to their int representations

--- bkbdtimesheet_backend/views.py
+++ bkbdtimesheet_backend/views.py
@@ -35,6 +35,8 @@ def submit(request):
 	requestInput = request.POST
 	date = str(requestInput['weekof']).split('/')
 	days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	timeMap = ['8:00 AM', '8:30 AM', '9:00 AM', '9:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM', '4:30 PM', '5:00 PM', '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM']
+	lunchTimeMap = ['11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM']
 	
 	with open('timesheet.csv', 'wb') as f:
 		grandTotal = 0
@@ -50,6 +52,12 @@ def submit(request):
 			timeOut = float(requestInput[str(days[i])+'TimeOut'])
 			timeTotal = timeOut - timeIn - (lunchOut - lunchIn)
 			grandTotal += timeTotal
+			
+			timeIn = timeMap[int(timeIn*2)]
+			lunchIn = timeMap[int(lunchIn*2)]
+			lunchOut = timeMap[int(lunchOut*2)]
+			timeOut = timeMap[int(timeOut*2)]
+			
 			writer.writerow([day, timeIn, lunchIn, lunchOut, timeOut, timeTotal])
 		writer.writerow(['','','','','', grandTotal])
 		
