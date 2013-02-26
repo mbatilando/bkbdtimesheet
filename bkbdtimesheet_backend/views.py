df34d31d383f14088bccb86dc830da5b144be2f3
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-02-26 15:36:58 -0800

    test

--- bkbdtimesheet_backend/views.py
+++ bkbdtimesheet_backend/views.py
@@ -43,7 +43,7 @@ def login(request):
 	print request.GET.get('callback', '')
 	print request.GET.get('username', 'username not here')
 	print request.GET.get('password', 'password not here')
-	print request.GET.items()[1]
+	print request.GET.items()[2]
 	print "THESE"
 	temp = json.loads(request.body)
 	print "USERNAME:"
