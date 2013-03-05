60dff388ee56de627ea34ae6ecdfc173d5151b59
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-03-05 11:04:07 -0800

    debugging

--- bkbdtimesheet_backend/views.py
+++ bkbdtimesheet_backend/views.py
@@ -81,7 +81,6 @@ def logout(request):
 @csrf_exempt	
 def submit(request):
 
-        """
         print("Breakpoint 0")
 	
 	#requestInput = request.POST
@@ -107,8 +106,7 @@ def submit(request):
 	lunchTimeMap = ['10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '1:00 PM', '1:30 PM', '2:00 PM', '2:30 PM', '3:00 PM', '3:30 PM', '4:00 PM']
 	manager = str(requestInput['manager'])
 	cc= str(requestInput['cc'])
-	"""
-        requestInput = request.GET
+
 
 	print("Breakpoint 2")
 	
