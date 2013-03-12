30a23deee73e89b36260799d6955daa28c878fb0
Mari Batilando <mari.batilando@blackbaud.com>
2013-03-11 17:41:45 -0700

    edited

--- timeSheet_view.rb
+++ timeSheet_view.rb
@@ -11,45 +11,45 @@ class timeSheetView
 
 
 	keyword(:MondayTimeIn) {browser.select_list(:name, "MondayTimeIn")}
-	keyword(:MondayTimeIn) {browser.select_list(:name, "MondayLunchIn")}
-	keyword(:MondayTimeIn) {browser.select_list(:name, "MondayLunchOut")}
-	keyword(:MondayTimeIn) {browser.select_list(:name, "MondayTimeOut")}
+	keyword(:MondayLunchIn) {browser.select_list(:name, "MondayLunchIn")}
+	keyword(:MondayLunchOut) {browser.select_list(:name, "MondayLunchOut")}
+	keyword(:MondayTimeOut) {browser.select_list(:name, "MondayTimeOut")}
 	keyword(:MondayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 5).text}
 
 	keyword(:TuesdayTimeIn) {browser.select_list(:name, "TuesdayTimeIn")}
-	keyword(:TuesdayTimeIn) {browser.select_list(:name, "TuesdayLunchIn")}
-	keyword(:TuesdayTimeIn) {browser.select_list(:name, "TuesdayLunchOut")}
-	keyword(:TuesdayTimeIn) {browser.select_list(:name, "TuesdayTimeOut")}
+	keyword(:TuesdayLunchIn) {browser.select_list(:name, "TuesdayLunchIn")}
+	keyword(:TuesdayLunchOut) {browser.select_list(:name, "TuesdayLunchOut")}
+	keyword(:TuesdayTimeOut) {browser.select_list(:name, "TuesdayTimeOut")}
 	keyword(:TuesdayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 11).text}
 
 	keyword(:WednesdayTimeIn) {browser.select_list(:name, "WednesdayTimeIn")}
-	keyword(:WednesdayTimeIn) {browser.select_list(:name, "WednesdayLunchIn")}
-	keyword(:WednesdayTimeIn) {browser.select_list(:name, "WednesdayLunchOut")}
-	keyword(:WednesdayTimeIn) {browser.select_list(:name, "WednesdayTimeOut")}
+	keyword(:WednesdayLunchIn) {browser.select_list(:name, "WednesdayLunchIn")}
+	keyword(:WednesdayLunchOut) {browser.select_list(:name, "WednesdayLunchOut")}
+	keyword(:WednesdayTimeOut) {browser.select_list(:name, "WednesdayTimeOut")}
 	keyword(:WednesdayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 17).text}
 
 	keyword(:ThursdayTimeIn) {browser.select_list(:name, "ThursdayTimeIn")}
-	keyword(:ThursdayTimeIn) {browser.select_list(:name, "ThursdayLunchIn")}
-	keyword(:ThursdayTimeIn) {browser.select_list(:name, "ThursdayLunchOut")}
-	keyword(:ThursdayTimeIn) {browser.select_list(:name, "ThursdayTimeOut")}
+	keyword(:ThursdayLunchIn) {browser.select_list(:name, "ThursdayLunchIn")}
+	keyword(:ThursdayLunchOut) {browser.select_list(:name, "ThursdayLunchOut")}
+	keyword(:ThursdayTimeOut) {browser.select_list(:name, "ThursdayTimeOut")}
 	keyword(:ThursdayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 23).text}
 
 	keyword(:FridayTimeIn) {browser.select_list(:name, "FridayTimeIn")}
-	keyword(:FridayTimeIn) {browser.select_list(:name, "FridayLunchIn")}
-	keyword(:FridayTimeIn) {browser.select_list(:name, "FridayLunchOut")}
-	keyword(:FridayTimeIn) {browser.select_list(:name, "FridayTimeOut")}
+	keyword(:FridayLunchIn) {browser.select_list(:name, "FridayLunchIn")}
+	keyword(:FridayLunchOut) {browser.select_list(:name, "FridayLunchOut")}
+	keyword(:FridayTimeOut) {browser.select_list(:name, "FridayTimeOut")}
 	keyword(:FridayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 29).text}
 
 	keyword(:SaturdayTimeIn) {browser.select_list(:name, "SaturdayTimeIn")}
-	keyword(:SaturdayTimeIn) {browser.select_list(:name, "SaturdayLunchIn")}
-	keyword(:SaturdayTimeIn) {browser.select_list(:name, "SaturdayLunchOut")}
-	keyword(:SaturdayTimeIn) {browser.select_list(:name, "SaturdayTimeOut")}
+	keyword(:SaturdayLunchIn) {browser.select_list(:name, "SaturdayLunchIn")}
+	keyword(:SaturdayLunchOut) {browser.select_list(:name, "SaturdayLunchOut")}
+	keyword(:SaturdayTimeOut) {browser.select_list(:name, "SaturdayTimeOut")}
 	keyword(:SaturdayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 35).text}
 
 	keyword(:SundayTimeIn) {browser.select_list(:name, "SundayTimeIn")}
-	keyword(:SundayTimeIn) {browser.select_list(:name, "SundayLunchIn")}
-	keyword(:SundayTimeIn) {browser.select_list(:name, "SundayLunchOut")}
-	keyword(:SundayTimeIn) {browser.select_list(:name, "SundayTimeOut")}
+	keyword(:SundayLunchIn) {browser.select_list(:name, "SundayLunchIn")}
+	keyword(:SundayLunchOut) {browser.select_list(:name, "SundayLunchOut")}
+	keyword(:SundayTimeOut) {browser.select_list(:name, "SundayTimeOut")}
 	keyword(:SundayTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 41).text}
 
 	keyword(:WeekTotal)  {browser.table(:index, 0).tbody(:index, 0).cell(:index, 46).text}
@@ -57,4 +57,16 @@ class timeSheetView
 	keyword(:submitButton) {browser.button(:name, 'submitBut2')}
 	keyword(:submitConfirm) {browser.button(:name, 'modalConfirm')}
 	keyword(:submitNegate) {browser.button(:name, 'cancelSub')}
-end
+
+	days =["Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday" ,"Sunday"]
+	
+	for day in days
+		timeIn = day + "TimeIn"
+		keyword(timeIn.to_sym)		{browser.select_list(:name, timeIn)}
+		lunchIn = day + "LunchIn"
+		keyword(lunchIn.to_sym)		{browser.select_list(:name. lunchIn)}
+		lunchOut = day + "LunchOut"
+		keyword(lunchOut.to_sym)	{browser.select_list(:name, lunchOut)}
+		timeOut = day + "TimeOut"
+		keyword(timeOut.to_sym)		{browser.select_list(:name, timeOut)}
+	end
