1882b073ccb41a63be47f5383c5680087b338855
Oscar Bachtiar <oscar.bachtiar@blackbaud.com>
2013-03-15 12:00:04 -0700

    added step definition for putting times

--- cucumber/bkbdtimesheet_steps.rb
+++ cucumber/bkbdtimesheet_steps.rb
@@ -8,9 +8,14 @@ Given /^I open the timesheet app at "([^"]*)" on Chrome/ do |url|
 end
 
 Given /^I input username "([^"]*)" and password "([^"]*)"/ do |username, password|
-  puts "hey yo"
   @browser.text_field(:id, 'login-username').set(username)
   @browser.text_field(:id, 'login-password').set(password)
   @browser.button(:id, 'loginBut').click
 end
 
+When /^I enter timeIn "([^"]*)", lunchIn "([^"]*)", lunchOut "([^"]*)", and timeOut "([^"]*)" for day "([^"]*)"/ do |timeIn, lunchIn, lunchOut, timeOut, day|
+  @browser.select_list(:id, day+"TimeIn").select(timeIn)
+  @browser.select_list(:id, day+"LunchIn").select(lunchIn)
+  @browser.select_list(:id, day+"LunchOut").selectt(lunchOut)
+  @browser.select_list(:id, day+"TimeOut").selectt(timeOut)
+end
