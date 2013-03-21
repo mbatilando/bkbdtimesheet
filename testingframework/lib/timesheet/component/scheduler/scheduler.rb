78c12b618e2af8ac0912717c28aa2c201661f63d
Ashley Gau <agau4779@gmail.com>
2013-03-21 16:38:23 -0700

    added skeleton methods for scheduler

--- testingframework/lib/timesheet/component/scheduler/scheduler.rb
+++ testingframework/lib/timesheet/component/scheduler/scheduler.rb
@@ -5,5 +5,20 @@ module Timesheet
     def submit
       @view.submitButton.click
     end
+
+    def enterTime()
+    end
+
+    def selectManager(manager)
+    end
+
+    def enterInternName(internName)
+    end
+
+    def enterEmail(email)
+    end
+
+    def enterDate(date)
+    end
   end
 end
